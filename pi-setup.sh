df2f1911492745e5ce5203076b3750c05cdea989
Adri Idzwan Mansor <adri.mcmc@gmail.com>
2021-02-02 21:02:07 +0800

    Update pi-setup.sh

--- pi-setup.sh
+++ pi-setup.sh
@@ -7,6 +7,7 @@
 ##   installs, make it easier to     ##
 ##   find common containers          ##
 ##   suitable for arm/pi             ##
+##   Containers are cross-platform   ##
 ##                                   ##
 ## Note: Code designed for           ##
 ##   ultra-wide monitors, sorry!     ##
@@ -15,22 +16,48 @@
 
 IP=`hostname  -I | cut -f1 -d' '`
 clear
-TITLE="\e[5m\n\n\n[--------------------------------------]\n[----------- \e[7mPi Setup Script\e[0m-----------]\n[---------------- \e[7mby C-Fu\e[0m--------------]\n[--------------------------------------]\n\e[0m"
-echo -e $TITLE
 
-sleep 3
+echo -e "\e[5m\n\n
+[--------------------------------------]
+[----------- \e[7mPi Setup Script\e[0m-----------]
+[---------------- \e[7mby C-Fu\e[0m--------------]
+[--------------------------------------]
+\e[0m"
+
+sleep 2
 clear
+#Clear vars
+instDocker=0
+instDockerCompose=0
+instRClone=0
+instPortainer=0
+instNginxProxy=0
+instOrganizr=0
 
-cmd=(dialog --separate-output --checklist "Select options:" 22 76 16)
+cmd=(dialog --separate-output 
+     --title "C-Fu's Pi setup script for common apps"
+     --checklist "Select software to install/update:
+     (WARNING: DO NOT RE-INSTALL DOCKER & DOCKER-COMPOSE THIS WAY!
+     Docker - Containerize your apps, it's the future!
+     RClone - mount your cloud storage easily
+     Portainer - manage your container the pro way
+     NginxProxyManager - add ssl to your DDNS!
+     Organizr - your personal start page to all of your web apps
+     WordPress - host your site from your house!
+     " 40 80 61
+    )
 options=(1 "Docker & Docker Compose" off    # any option can be set to default to "on"
          2 "RClone - mount your GDrive One Drive MEGA AWS WEBDav etc" off
          3 "[Docker] Portainer - manage containers" off
          4 "[Docker] NginxProxyManager - reverse-proxy your containers" off
          5 "[Docker] Organizr - your personal start page" off
-         6 "[Docker] WordPress (Not yet)" off)
+         6 "[Docker] WordPress (Not yet)" off
+         )
          
 choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 clear
+
+
 
 for choice in $choices
 do
@@ -38,15 +65,15 @@ do
         1)
             echo -e "\n\n\e[33m[------------------------------------]\n[ Installing Docker & Docker Compose ]\n[------------------------------------]"
             #Install docker
-            curl -fsSL https://get.docker.com -o get-docker.sh && instDocker=1      || echo -e "Error! Could not get docker script!" 										&& instDocker=0
-            sudo sh get-docker.sh                              && instDocker=1      || echo -e "Error! Could not run script!" 												&& instDocker=0
+            curl -fsSL https://get.docker.com -o get-docker.sh && instDocker=1      || echo -e "Error! Did not get docker script!" 										&& instDocker=0
+            sudo sh get-docker.sh                              && instDocker=1      || echo -e "Error! Did not run script!" 												&& instDocker=0
             sudo usermod -aG docker pi || sudo usermod -aG docker dietpi #just in case
             #Install docker-compose
             sudo apt install -y libffi-dev libssl-dev 
             sudo apt install -y python3 python3-pip
             sudo apt remove  -y python-configparser #not needed
             echo -e "\e[33mInstalling Docker Compose. Will take some time..."
-            sudo pip3 -v install docker-compose && instDockerCompose=1              || echo -e "Error! Could not install docker-compose!" 									&& instDockerCompose=0
+            sudo pip3 -v install docker-compose && instDockerCompose=1              || echo -e "Error! Did not install docker-compose!" 									&& instDockerCompose=0
             echo -e "\n\n"
             if [ "$instDocker"="1" ]; then
                 echo -e "\e[33m[Docker] install         : DONE!"
@@ -59,12 +86,17 @@ do
                 echo -e "\e[31m[Docker Compose] install : FAILED!"
             fi
             echo -e "\n\n"
+            sleep 2
             ;;
         2)
-            echo -e "\n\n\e[33m[------------------------------------]\n[          Installing RClone         ]\n[------------------------------------]"
+            echo -e "\n\n\e[33m
+            [------------------------------------]
+            [          Installing RClone         ]
+            [------------------------------------]
+            "
             #Install rclone
             echo "\e[33mInstalling rclone..."
-            curl https://rclone.org/install.sh | sudo bash       && instRClone=1    || echo -e "\e[31mError! Could not download RClone! Check your Internet connection! " 	&& instRClone=0 
+            curl https://rclone.org/install.sh | sudo bash       && instRClone=1    || echo -e "\e[31mError! Did not download RClone! Check your Internet connection! " 	&& instRClone=0 
             echo -e "\n\n"   
             if [ "$instRClone"="1" ]; then
                 echo -e "\e[33m[RClone] install : DONE!"
@@ -72,9 +104,14 @@ do
                 echo -e "\e[31m[RClone] install : FAILED!"
             fi
             echo -e "\n\n"
+            sleep 2
             ;; 
         3)
-            echo -e "\n\n\e[33m[------------------------------------]\n[        Installing Portainer        ]\n[------------------------------------]"
+            echo -e "\n\n\e[33m
+            [------------------------------------]
+            [        Installing Portainer        ]
+            [------------------------------------]
+            "
             #Install Portainer
             docker run -d                   				\
               --name=portainer 								\
@@ -90,6 +127,7 @@ do
                 echo -e "\e[31m[Portainer] install : FAILED!"
             fi
             echo -e "\n\n" 
+            sleep 2
             ;;
         4)
             echo -e "\n\n\e[33m[------------------------------------]\n[    Installing NginxProxyManager    ]\n[------------------------------------]"
@@ -116,40 +154,47 @@ do
             else
                 echo -e "\e[31m[Organizr] cannot be started! Check the docker-compose file!" 
             fi
+            sleep 2
             ;;
     esac
 done
 
+echo -e "\n\e[32m
+[------------------------------------]
+[              ALL DONE!             ] 
+[  https://github.com/C-Fu/pi-setup  ]
+[------------------------------------]
+\e[39m\n"
+#echo -e "\e[32mCheck running containers..."
+#docker ps
 
 
-echo -e "\e[32mALL DONE! 
-Check running containers..."
-docker ps
 
-if [ instDocker=1 ];then 
+if [ "instDocker" = "1" ] ; then 
   echo -e "\e[32m[INSTALLED] \e[33m[Docker]\e[39m can be run by using 
-             \e[33mdocker run -d --name something -p hostportno:containerport -v hostdir:containerdir repo/container\e[39m 
+             \e[33mdocker run -d --name something -p hostportno:containerport 
+             -v hostdir:containerdir repo/container\e[39m (one line)
              then open up browser at \e[34m$IP:HostPortNo\e[39m" 
 fi
 
-if [ instDockerCompose=1 ];then
+if [ "instDockerCompose" = "1" ];then
   echo -e "\e[32m[INSTALLED] \e[33m[Docker Compose]\e[39m can be run by 
              editing your \e[33mdocker-compose.yml\e[39m file, then do 
              \e[33mdocker-compose up -d\e[39m to start, or 
              \e[33mdocker-compose down --rmi local\e[39m to uninstall and remove container"
 fi
 
-if [ instRClone=1 ];then
+if [ "instRClone" = "1" ];then
   echo -e "\e[32m[INSTALLED] \e[33m[RClone]\e[39m can be run by using
              \e[33mrclone config\e[39m, and then \e[33mrclone cp\e[39m, \e[33mrclone mv\e[39m, 
              \e[33mrclone lsd\e[39m, \e[33mrclone ls\e[39m, \e[33mrclone mount\e[39m."
 fi
 
-if [ instPortainer=1 ];then
+if [ "instPortainer" = "1" ];then
   echo -e "\e[32m[INSTALLED] \e[33m[portainer]\e[39m deployed at \e[34m$IP:9000\e[39m."
 fi
 
-if [ instNginxProxy=1 ];then
+if [ "instNginxProxy" = "1" ];then
   echo -e "\e[32m[INSTALLED] \e[33m[nginx-proxy-manager]\e[39m deployed at \e[34m$IP:181\e[39m.  
              Your IP is \e[34m$IP\e[39m.  
              Port-forward/Add virtual server port 80 and 443 to \e[34m$IP:180\e[39m and \e[34m$IP:1443\e[39m   
@@ -157,7 +202,7 @@ if [ instNginxProxy=1 ];then
              \e[34m192.168.0.1 \e[39mor \e[34m192.168.1.1 \e[39mor \e[34m192.168.0.254\e[39m."
 fi
 
-if [ instOrganizr=1 ]; then
+if [ "instOrganizr" = "1" ]; then
   echo -e "\e[32m[INSTALLED] \e[33m[organizr]\e[39m deployed at \e[34m$IP:88\e[39m.   
              Port-forward/Add virtual server   
              port \e[34m80\e[39m to \e[34m$IP:88\e[39m  
